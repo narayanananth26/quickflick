99b07fc2f919f8d615b2931f5402bbb9588803de
Ananth Narayan <narayanananth26@gmail.com>
2023-06-16 12:11:51 +0530

    finish start api

--- src/index.js
+++ src/index.js
@@ -1,14 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { useState } from "react";
 // import './index.css';
 // import App from './App';
 
 import StarRating from "./StarRating";
 
+function Test() {
+	const [movieRating, setMovieRating] = useState(0);
+
+	return (
+		<div>
+			<StarRating
+				color="blue"
+				maxRating={10}
+				onSetRating={setMovieRating}
+			/>
+			<p>This movie was rated {movieRating} stars</p>
+		</div>
+	);
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-		<StarRating maxRating={5} />
-		<StarRating maxRating={10} />
+		<StarRating
+			maxRating={5}
+			messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+		/>
+		<StarRating size={24} color="red" defaultRating={2} />
+		<Test />
 	</React.StrictMode>
 );
