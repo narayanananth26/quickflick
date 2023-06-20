45b1e549e22ede2f1f6c9abb67d55f84fd91d720
Ananth Narayan <narayanananth26@gmail.com>
2023-06-20 15:50:01 +0530

    fetch movies from omdb api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const tempMovieData = [
 	{
@@ -45,9 +45,17 @@ const tempWatchedData = [
 const average = (arr) =>
 	arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
+const KEY = "d3da3e17";
+
 export default function App() {
 	const [movies, setMovies] = useState(tempMovieData);
 	const [watched, setWatched] = useState(tempWatchedData);
+
+	useEffect(function () {
+		fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`)
+			.then((res) => res.json())
+			.then((data) => setMovies(data.Search));
+	}, []);
 
 	return (
 		<>
